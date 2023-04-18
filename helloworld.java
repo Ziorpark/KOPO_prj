08dc442d61a07ea051fcd848c601e7eef85103cc
DESKTOP-BP71QIA\정해석 <tiya516@naver.com>
2023-04-18 14:29:33 +0900

    no message

--- helloworld.java
+++ helloworld.java
@@ -1,13 +1,9 @@
-public {
 
-    public static void main(String[] args) {
-        
-        String s1 = "hello world!";
-        System.out.println(s1);
-        System.out.println(s1);
-        System.out.println(s1);
+public static void main(String[] args) {
+    
+    String s1 = "hello world!";
+    System.out.println(s1);
+    System.out.println(s1);
+    System.out.println(s1);
 
-
-
-    }
 }
