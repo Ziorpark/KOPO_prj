2ed4696698d76b897b66a90a026b5912beef0229
DESKTOP-BP71QIA\정해석 <tiya516@naver.com>
2023-04-18 14:32:07 +0900

    헬로월드 추가

--- helloworld.java
+++ helloworld.java
@@ -2,8 +2,9 @@
 public static void main(String[] args) {
     
     String s1 = "hello world!";
+    String s2 = "★heloo world★";
     System.out.println(s1);
-    System.out.println(s1);
+    System.out.println(s2);
     System.out.println(s1);
 
 }
