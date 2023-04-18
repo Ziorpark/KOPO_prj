13a1d82ad3b0771b2487722cb5e621ad62e312d6
DESKTOP-BP71QIA\정해석 <tiya516@naver.com>
2023-04-18 14:12:21 +0900

    커밋 5번 다시하기

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,13 @@
-public static void main(String[] args) {
-    
-    String s1 = "hello world!";
-    System.out.println(s1);
-    System.out.println(s1);
-    System.out.println(s1);
+public {
 
+    public static void main(String[] args) {
+        
+        String s1 = "hello world!";
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+
+
+
+    }
 }
